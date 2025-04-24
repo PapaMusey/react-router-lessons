0ef98295eb77e931ba37dae46df6095068903e86
PapaMusey <p.emusey@gmail.com>
2025-04-24 12:11:53 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,31 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Contact from "./pages/Contact";
-
-import { Routes, Route } from "react-router-dom";
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import About from "./pages/About";
+import RootLayout from "./layout/RootLayout";
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+  );
+
   return (
-    <>
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div> 
-    </>
+  
+      <RouterProvider router={router} />
+      
   );
 };
 
