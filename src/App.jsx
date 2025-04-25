847ac0e2f75e5aa8fd09f00807aae166b17c8afc
PapaMusey <p.emusey@gmail.com>
2025-04-25 09:12:17 +0000

    Using Nested Routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
-import Contact from "./pages/Contact";
+import ContactInfo from "./components/ContactInfo";
 import {
   RouterProvider,
   Route,
@@ -11,24 +11,28 @@ import {
 } from "react-router-dom";
 import About from "./pages/About";
 import RootLayout from "./layout/RootLayout";
+import ContactForm from "./components/ContactForm";
+import ContactLayout from "./layout/ContactLayout";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-          <Route index element={<Home />} />
-          <Route path="products" element={<Products />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-    </Route>
-  )
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="products" element={<Products />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<ContactLayout />}>
+          <Route path="info" element={<ContactInfo />} />
+          <Route path="form" element={<ContactForm />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    )
   );
 
-  return (
-  
-      <RouterProvider router={router} />
-      
-  );
+  return <RouterProvider router={router} />;
 };
+
 
 export default App;
---
+++ src/components/ContactInfo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactInfo = () => {
+  return (
+    <div>
+     <div>Email: example@gamil.com</div>
+     <div>Phone +1234567890</div>
+     <div>Address: Lympac, Ghana</div>
+    </div>
+  )
+}
+
+export default ContactInfo
